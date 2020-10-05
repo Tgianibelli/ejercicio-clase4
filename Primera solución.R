556a02eb1c3f2476b96aba4e9c0805a11c12c216
Tgianibelli <t.j.gianibelli@gmail.com>
2020-10-05 16:48:05 -0300

    Primera solución

---
+++ Primera solución.R
@@ -0,0 +1,22 @@
+# Prendemos librerias 
+
+library(tidyverse)
+library(polAr)
+
+# Descargamos datos y los guardamos como objeto
+
+arg <- get_election_data("arg", "presi", "gral", 2003) %>% 
+  get_names()
+
+arg %>% 
+  filter(name_prov == "TUCUMAN") %>% 
+  mutate(total = sum(votos)) %>%  
+  mutate(votospct = (votos / total) * 100) %>% 
+  arrange(votospct) %>% 
+  mutate(dif = votospct[20] - votospct[19])
+  
+  
+  
+  
+
+
