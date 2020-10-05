47b11b74a15e28b0cba2a05aac868c73c68a9a35
Tgianibelli <t.j.gianibelli@gmail.com>
2020-10-05 16:58:46 -0300

    Cambios

--- Primera solución.R
+++ Primera solución.R
@@ -8,6 +8,8 @@ library(polAr)
 arg <- get_election_data("arg", "presi", "gral", 2003) %>% 
   get_names()
 
+# Filtramos para llegar a diferencia de votos 
+
 arg %>% 
   filter(name_prov == "TUCUMAN") %>% 
   mutate(total = sum(votos)) %>%  
